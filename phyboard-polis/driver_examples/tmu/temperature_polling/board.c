7731b6a6e381e4d3cf6c0cc2db1521f1cf39b620
Yashwanth Varakala <y.varakala@phytec.de>
2023-05-17 13:38:05 +0200

    phyboard-polis: tmu/temperature_polling: Board specific changes
    Update readme and remove unused audio, video clocks.
    
    Signed-off-by: Yashwanth Varakala <y.varakala@phytec.de>
    Signed-off-by: Cem Tenruh <c.tenruh@phytec.de>

--- phyboard-polis/driver_examples/tmu/temperature_polling/board.c
+++ phyboard-polis/driver_examples/tmu/temperature_polling/board.c
@@ -1,5 +1,6 @@
 /*
  * Copyright 2018-2021 NXP
+ * Copyright 2023 PHYTEC Messtechnik GmbH
  * All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
@@ -189,7 +190,4 @@ void BOARD_RdcInit(void)
     CLOCK_ControlGate(kCLOCK_SysPll1Gate, kCLOCK_ClockNeededAll);   /* Enable the CCGR gate for SysPLL1 in Domain 1 */
     CLOCK_ControlGate(kCLOCK_SysPll2Gate, kCLOCK_ClockNeededAll);   /* Enable the CCGR gate for SysPLL2 in Domain 1 */
     CLOCK_ControlGate(kCLOCK_SysPll3Gate, kCLOCK_ClockNeededAll);   /* Enable the CCGR gate for SysPLL3 in Domain 1 */
-    CLOCK_ControlGate(kCLOCK_AudioPll1Gate, kCLOCK_ClockNeededAll); /* Enable the CCGR gate for AudioPLL1 in Domain 1 */
-    CLOCK_ControlGate(kCLOCK_AudioPll2Gate, kCLOCK_ClockNeededAll); /* Enable the CCGR gate for AudioPLL2 in Domain 1 */
-    CLOCK_ControlGate(kCLOCK_VideoPll1Gate, kCLOCK_ClockNeededAll); /* Enable the CCGR gate for VideoPLL1 in Domain 1 */
 }
