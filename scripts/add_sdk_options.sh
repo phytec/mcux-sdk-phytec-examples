1d06e438801f8ddf73fec22693ebb6b2a212973a
Yannic Moog <y.moog@phytec.de>
2023-05-30 13:26:20 +0200

    !fixup: scripts: add script to modify sdk paths in .sh and CMakeLists files
    The sed script file used to hardcode the paths to be replaced, however
    not all examples are at the same depth and thus the script does not work
    for all examples. Remove the sed file and put the sed commands into the
    .sh file.
    
    Signed-off-by: Yannic Moog <y.moog@phytec.de>
    Signed-off-by: Cem Tenruh <c.tenruh@phytec.de>

--- scripts/add_sdk_options.sh
+++ scripts/add_sdk_options.sh
@@ -1,3 +1,32 @@
 #!/usr/bin/bash
 
-find . -path "*/CMakeLists.txt" -o -path "*/*.sh" | xargs sed -i -f scripts/rm_hardcoded_sdk.sed
+files=$(find . -path "*/*.sh")
+for filename in ${files}; do
+    replace_str=$(rg -o -I -N '(\.\.)?/\.\.(/\.\.)+' $filename | uniq)
+    if [ -n "$replace_str" ]; then
+        sed -i '/^cmake.* -DCMAKE_TOOLCHAIN_FILE.*/i\
+	    if [ -n ${MCUX_SDK_PATH} ]; then\
+		sdk_dir=${MCUX_SDK_PATH};\
+            else sdk_dir='${replace_str}'\
+            fi' ${filename}
+
+        replace_str=$(echo $replace_str | sed 's/\//\\\//g')
+
+        sed -i 's/CMAKE_TOOLCHAIN_FILE="'${replace_str}'/CMAKE_TOOLCHAIN_FILE="${sdk_dir}/' ${filename}
+    fi
+done
+
+files=$(find . -path "*/CMakeLists.txt")
+
+for filename in ${files}; do
+    replace_str=$(rg -o -I -N '(\.\.)?/\.\.(/\.\.)+' $filename | uniq | sed 's/\//\\\//g')
+    if [ -n "$replace_str" ]; then
+
+    sed -i 's/SET(SdkRootDirPath ${ProjDirPath}'${replace_str}')/if (DEFINED ENV{MCUX_SDK_PATH})\
+        SET(SdkRootDirPath $ENV{MCUX_SDK_PATH})\
+    else()\
+        SET(SdkRootDirPath ${ProjDirPath}'${replace_str}')\
+        endif()/' ${filename}
+
+fi
+done
