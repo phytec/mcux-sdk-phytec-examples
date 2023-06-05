44256ad2742b3db482c9ac3ff4a8818a7135df1f
Yannic Moog <y.moog@phytec.de>
2023-06-05 08:37:12 +0200

    phyboard-pollux: gpt_timer example: adapt NXP demo to phyboard-pollux
    Remove unused audio/video code and adapt readme to phyboard-pollux
    instead of evk board.
    
    Signed-off-by: Yannic Moog <y.moog@phytec.de>
    Signed-off-by: Cem Tenruh <c.tenruh@phytec.de>

--- phyboard-pollux/driver_examples/gpt/timer/board.h
+++ phyboard-pollux/driver_examples/gpt/timer/board.h
@@ -1,5 +1,6 @@
 /*
  * Copyright 2019-2020 NXP
+ * Copyright 2023 PHYTEC Messtechnik GmbH
  * All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
@@ -9,13 +10,12 @@
 #define _BOARD_H_
 #include "clock_config.h"
 #include "fsl_clock.h"
-#include "fsl_audiomix.h"
 /*******************************************************************************
  * Definitions
  ******************************************************************************/
 /*! @brief The board name */
-#define BOARD_NAME        "MIMX8MP-EVK"
-#define MANUFACTURER_NAME "NXP"
+#define BOARD_NAME        "phyBOARD-Pollux"
+#define MANUFACTURER_NAME "PHYTEC"
 #define BOARD_DOMAIN_ID   (1U)
 /* The UART to use for debug messages. */
 #define BOARD_DEBUG_UART_TYPE     kSerialPort_Uart
---
+++ phyboard-pollux/driver_examples/gpt/timer/readme.rst
@@ -0,0 +1,31 @@
+Overview
+========
+
+The gpt_timer project is a simple demonstration program of the SDK GPT driver.
+It sets up the GPT hardware block to trigger a periodic interrupt after every 1
+second. When the GPT interrupt is triggered a message a printed on the UART
+terminal.
+
+Running the demo
+================
+When the example runs successfully, following information can be seen on the
+terminal:
+
+~~~~~~~~~~~~~~~~~~~~~
+
+Press any key to start the example
+s
+Starting GPT timer ...
+ GPT interrupt occurred!
+ GPT interrupt occurred!
+ GPT interrupt occurred!
+ GPT interrupt occurred!
+ .
+ .
+ .
+ GPT interrupt occurred!
+ .
+ .
+ .
+
+~~~~~~~~~~~~~~~~~~~~~
