3d1db1cd9b4c8baf94408405359974555b874eb7
Steffen Hemer <s.hemer@phytec.de>
2023-08-01 17:15:24 +0200

    scripts: add_sdk_options: Add checks for not running it multiple times, corrected indentation of inserts
    This adds checks if the SDK option were already applied to the examples
    as multiple runs would lead to funny multi-replacements. The
    script itself is now indented with tabs only and the replacements part's
    indentation matches the goal scripts.
    
    Signed-off-by: Steffen Hemer <s.hemer@phytec.de>
    Signed-off-by: Dominik Haller <d.haller@phytec.de>

--- scripts/add_sdk_options.sh
+++ scripts/add_sdk_options.sh
@@ -1,32 +1,46 @@
 #!/usr/bin/bash
 
-files=$(find . -path "*/*.sh")
+echo "===== Add options to build_*.sh ====="
+
+files=$(find . -path "*armgcc/build_*.sh") # exclude scripts folder
 for filename in ${files}; do
-    replace_str=$(rg -o -I -N '(\.\.)?/\.\.(/\.\.)+' $filename | uniq)
-    if [ -n "$replace_str" ]; then
-        sed -i '/^cmake.* -DCMAKE_TOOLCHAIN_FILE.*/i\
-	    if [ -n ${MCUX_SDK_PATH} ]; then\
-		sdk_dir=${MCUX_SDK_PATH};\
-            else sdk_dir='${replace_str}'\
-            fi' ${filename}
+	if ! grep -m 1 -q "MCUX_SDK_PATH" "$filename"; then
+		replace_str=$(rg -o -I -N '(\.\.)?/\.\.(/\.\.)+' $filename | uniq)
+		if [ -n "$replace_str" ]; then
+			echo "Replace SDK path in $filename"
+			sed -i '/^cmake.* -DCMAKE_TOOLCHAIN_FILE.*/i\
+if [ -n ${MCUX_SDK_PATH} ]; then\
+    sdk_dir=${MCUX_SDK_PATH}\
+else\
+    sdk_dir='${replace_str}'\
+fi' ${filename}
 
-        replace_str=$(echo $replace_str | sed 's/\//\\\//g')
+			replace_str=$(echo $replace_str | sed 's/\//\\\//g')
 
-        sed -i 's/CMAKE_TOOLCHAIN_FILE="'${replace_str}'/CMAKE_TOOLCHAIN_FILE="${sdk_dir}/' ${filename}
-    fi
+			sed -i 's/CMAKE_TOOLCHAIN_FILE="'${replace_str}'/CMAKE_TOOLCHAIN_FILE="${sdk_dir}/' ${filename}
+		fi
+	else
+		echo "$filename already w replaced SDK path"
+	fi
 done
+
+echo "===== Add options to CMakeLists ====="
 
 files=$(find . -path "*/CMakeLists.txt")
 
 for filename in ${files}; do
-    replace_str=$(rg -o -I -N '(\.\.)?/\.\.(/\.\.)+' $filename | uniq | sed 's/\//\\\//g')
-    if [ -n "$replace_str" ]; then
-
-    sed -i 's/SET(SdkRootDirPath ${ProjDirPath}'${replace_str}')/if (DEFINED ENV{MCUX_SDK_PATH})\
+	if ! grep -m 1 -q "MCUX_SDK_PATH" "$filename"; then
+	replace_str=$(rg -o -I -N '(\.\.)?/\.\.(/\.\.)+' $filename | uniq | sed 's/\//\\\//g')
+		if [ -n "$replace_str" ]; then
+			echo "Replace SDK path in $filename"
+			sed -i 's/SET(SdkRootDirPath ${ProjDirPath}'${replace_str}')/if (DEFINED ENV{MCUX_SDK_PATH})\
         SET(SdkRootDirPath $ENV{MCUX_SDK_PATH})\
     else()\
         SET(SdkRootDirPath ${ProjDirPath}'${replace_str}')\
-        endif()/' ${filename}
+    endif()/' ${filename}
 
-fi
+		fi
+	else
+		echo "$filename already w replaced SDK path"
+	fi
 done
