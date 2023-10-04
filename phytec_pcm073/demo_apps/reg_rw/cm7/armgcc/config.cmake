afbab3174ad945209f9866d850708044548c2523
Steffen Hemer <s.hemer@phytec.de>
2023-10-04 09:34:44 +0200

    phytec_pcm073: demo_apps: reg_rw: Remove mcu id from cmake includes, add 'core' to cmake moduel paths, add MCUX_DEVICE to cmake config.
    Signed-off-by: Steffen Hemer <s.hemer@phytec.de>

--- phytec_pcm073/demo_apps/reg_rw/cm7/armgcc/config.cmake
+++ phytec_pcm073/demo_apps/reg_rw/cm7/armgcc/config.cmake
@@ -1,2 +1,3 @@
 # config to select component, the format is CONFIG_USE_${component}
-set(CONFIG_USE_driver_lpuart_MIMXRT1176_cm7 true)
+set(CONFIG_USE_driver_lpuart true)
+set(MCUX_DEVICE "MIMXRT1176")
